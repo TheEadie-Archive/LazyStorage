6021e591806fe7ce527662ac389b658ca071cf62
David Eadie <david@eadie.net>
2015-04-07 20:03:53 +0100

    In memory implementation of storage interfaces

---
+++ LazyLibrary/Storage/Memory/MemoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LazyLibrary.Storage.Memory
+{
+    internal class MemoryRepository<T> : IRepository<T> where T : IStorable
+    {
+        private Dictionary<int, T> repository = new Dictionary<int, T>();
+
+        public T GetById(int id)
+        {
+            return this.repository.ContainsKey(id) ? this.repository[id] : default(T);
+        }
+
+        public void Insert(T item)
+        {
+            int nextId = this.repository.Keys.Max() + 1;
+            item.Id = nextId;
+            this.repository.Add(item.Id, item);
+        }
+
+        public void Update(T item)
+        {
+            this.repository.Remove(item.Id);
+            this.repository.Add(item.Id, item);
+        }
+
+        public void Delete(T item)
+        {
+            this.repository.Remove(item.Id);
+        }
+    }
+}
---
+++ LazyLibrary/Storage/Memory/MemoryStorage.cs
@@ -0,0 +1,18 @@
+﻿namespace LazyLibrary.Storage.Memory
+{
+    internal class MemoryStorage : IStorage
+    {
+        public IRepository<T> GetRepository<T>() where T : IStorable
+        {
+            return new MemoryRepository<T>();
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Discard()
+        {
+        }
+    }
+}
