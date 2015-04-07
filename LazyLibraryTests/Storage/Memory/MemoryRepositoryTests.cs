1e62a8be8e34eeee937c793a6963cb49752f06bd
David Eadie <david@eadie.net>
2015-04-07 22:24:11 +0100

    Ran cleanup on test file.

--- LazyLibraryTests/Storage/Memory/MemoryRepositoryTests.cs
+++ LazyLibraryTests/Storage/Memory/MemoryRepositoryTests.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using LazyLibrary.Storage;
+﻿using LazyLibrary.Storage;
 using LazyLibrary.Storage.Memory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
 
 namespace LazyLibrary.Tests.Storage.Memory
 {
@@ -15,15 +15,20 @@ namespace LazyLibrary.Tests.Storage.Memory
             var repo = new MemoryRepository<TestObject>();
             var obj = new TestObject();
 
-
             repo.Upsert(obj);
 
-            Assert.IsTrue(repo.Get().Any());
+            Assert.IsTrue(repo.Get().Any(), "The object could not be added to the repository");
         }
     }
 
     public class TestObject : IStorable
     {
         public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
     }
 }
