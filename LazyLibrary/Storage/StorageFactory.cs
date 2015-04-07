61b692a86332fc44d0a8ce5abd29163726e686a4
David Eadie <david@eadie.net>
2015-04-07 20:05:14 +0100

    Factory class to get implementations of storage interfaces

---
+++ LazyLibrary/Storage/StorageFactory.cs
@@ -0,0 +1,23 @@
+﻿using LazyLibrary.Storage.Memory;
+
+namespace LazyLibrary.Storage
+{
+    public class StorageFactory
+    {
+        private IStorage store;
+
+        public StorageFactory()
+        {
+        }
+
+        public IStorage GetStore()
+        {
+            if (this.store != null)
+            {
+                this.store = new MemoryStorage();
+            }
+
+            return this.store;
+        }
+    }
+}
