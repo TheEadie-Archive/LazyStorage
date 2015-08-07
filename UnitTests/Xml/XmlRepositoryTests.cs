3d0747d9ebe3c7def2cd553bd3f660d0f2fcd480
David Eadie <david.eadie@red-gate.com>
2015-08-07 16:51:35 +0100

    Red: Can get an object by linq statement

--- UnitTests/Xml/XmlRepositoryTests.cs
+++ UnitTests/Xml/XmlRepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Xml.Linq;
+using LazyStorage.InMemory;
 using LazyStorage.Xml;
 using Xunit;
 
@@ -16,6 +17,22 @@ namespace LazyStorage.Tests.Xml
             repo.Upsert(obj);
 
             Assert.True(repo.Get().Any(), "The object could not be added to the repository");
+        }
+
+        [Fact]
+        public void CanGetByLinq()
+        {
+            var repo = new XmlRepository<TestObject>(new XDocument(new XElement("Root")));
+            var objOne = new TestObject {Name = "one"};
+            var objTwo = new TestObject {Name = "two"};
+
+            repo.Upsert(objOne);
+            repo.Upsert(objTwo);
+
+            var result = repo.Get(x => x.Name == "one").SingleOrDefault();
+
+            Assert.NotNull(result);
+            Assert.True(result.Equals(objOne), "The object could not be retrieved from the repository");
         }
     }
 }
