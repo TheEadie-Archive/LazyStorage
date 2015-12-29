04512b7b1bd8092d3d047b021080a0a8fa8480d3
David Eadie <david.eadie@red-gate.com>
2015-12-29 16:53:03 +0000

    Red: Test that we can add to a repo with a converter

---
+++ UnitTests/RepositoryWithConverterTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LazyStorage.Tests.StorageTypes;
+using Xunit;
+
+namespace LazyStorage.Tests
+{
+    public class RepositoryWithConverterTests
+    {
+        public static IEnumerable<object[]> StorageTypes => new[]
+        {
+            new object[] {new InMemoryTestStorage(), },
+        };
+
+        private ITestStorage m_CurrentStorage;
+
+        [Theory, MemberData("StorageTypes")]
+        public void CanAddToRepo(ITestStorage storage)
+        {
+            m_CurrentStorage = storage;
+
+            var converter = new TestObjectStorageConverter();
+
+            var repo = storage.GetStorage().GetRepository(converter);
+
+            var obj = new TestObjectNotIStorable();
+            obj.Name = "Test";
+            obj.StartDate = DateTime.Now;
+            obj.EndDate = DateTime.Now;
+
+            repo.Upsert(obj);
+            
+            var repoObj = repo.Get().Single();
+
+            Assert.True(repoObj.ContentEquals(obj), "The object returned does not match the one added");
+        }
+        
+        public void Dispose()
+        {
+            m_CurrentStorage.CleanUp();
+        }
+    }
+}
