7cbb7215e0d905011bce6f8a6d4818258eee8049
David Eadie <david.eadie@red-gate.com>
2016-05-19 16:26:05 +0100

    Remove Id from storable object

--- LazyStorage/StorableObject.cs
+++ LazyStorage/StorableObject.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 
 namespace LazyStorage
 {
-    public class StorableObject : IEquatable<StorableObject>
+    public class StorableObject
     {
-        public int LazyStorageInternalId { get; set; }
         public Dictionary<string, string> Info { get; }
 
         public StorableObject()
         {
             Info = new Dictionary<string, string>();
-        }
-        
-        public bool Equals(StorableObject other)
-        {
-            return (other.LazyStorageInternalId == LazyStorageInternalId);
         }
     }
 }
