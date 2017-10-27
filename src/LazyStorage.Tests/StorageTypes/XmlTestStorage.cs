a2c143a0ea618b151703ec36682c2206f0401109
David Eadie <david.eadie@red-gate.com>
2017-10-27 16:45:43 +0100

    R# rename field

--- src/LazyStorage.Tests/StorageTypes/XmlTestStorage.cs
+++ src/LazyStorage.Tests/StorageTypes/XmlTestStorage.cs
@@ -7,11 +7,11 @@ namespace LazyStorage.Tests.StorageTypes
 {
     public class XmlTestStorage : ITestStorage
     {
-        private readonly IStorage m_Storage = StorageFactory.GetXmlStorage(@"");
+        private readonly IStorage _storage = StorageFactory.GetXmlStorage(@"");
 
         public IStorage GetStorage()
         {
-            return m_Storage;
+            return _storage;
         }
 
         public void CleanUp()
