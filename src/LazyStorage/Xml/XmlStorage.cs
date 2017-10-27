d5e2545d509cb77a2e7e58c7c7f7927c8100705d
David Eadie <david.eadie@red-gate.com>
2017-10-27 16:41:04 +0100

    R# rename fields

--- src/LazyStorage/Xml/XmlStorage.cs
+++ src/LazyStorage/Xml/XmlStorage.cs
@@ -8,41 +8,41 @@ namespace LazyStorage.Xml
 {
     internal class XmlStorage : IStorage
     {
-        private readonly string m_StorageFolder;
-        private Dictionary<string, IRepository> m_Repos;
+        private readonly string _storageFolder;
+        private Dictionary<string, IRepository> _repos;
 
         public XmlStorage(string storageFolder)
         {
-            m_StorageFolder = storageFolder;
-            m_Repos = new Dictionary<string, IRepository>();
+            _storageFolder = storageFolder;
+            _repos = new Dictionary<string, IRepository>();
         }
 
         public IRepository<T> GetRepository<T>() where T : IStorable<T>, new()
         {
             var typeAsString = typeof(T).ToString();
 
-            if (!m_Repos.ContainsKey(typeAsString))
+            if (!_repos.ContainsKey(typeAsString))
             {
-                m_Repos.Add(typeAsString, new XmlRepository<T>(m_StorageFolder));
+                _repos.Add(typeAsString, new XmlRepository<T>(_storageFolder));
             }
 
-            return m_Repos[typeAsString] as IRepository<T>;
+            return _repos[typeAsString] as IRepository<T>;
         }
 
         public IRepository<T> GetRepository<T>(IConverter<T> converter)
         {
             var typeAsString = typeof(T).ToString();
 
-            if (!m_Repos.ContainsKey(typeAsString))
+            if (!_repos.ContainsKey(typeAsString))
             {
-                m_Repos.Add(typeAsString, new XmlRepositoryWithConverter<T>(m_StorageFolder, converter));
+                _repos.Add(typeAsString, new XmlRepositoryWithConverter<T>(_storageFolder, converter));
             }
 
-            return (IRepository<T>)m_Repos[typeAsString];
+            return (IRepository<T>)_repos[typeAsString];
         }
         public void Save()
         {
-            foreach (var repository in m_Repos)
+            foreach (var repository in _repos)
             {
                 repository.Value.Save();
             }
@@ -50,11 +50,11 @@ namespace LazyStorage.Xml
 
         public void Discard()
         {
-            foreach (var repository in m_Repos)
+            foreach (var repository in _repos)
             {
                 repository.Value.Load();
             }
-            m_Repos = new Dictionary<string, IRepository>();
+            _repos = new Dictionary<string, IRepository>();
         }
     }
 }
