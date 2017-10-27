4f40443d32a5d7427ed537fad360c7d5db2163be
David Eadie <david.eadie@red-gate.com>
2017-10-27 16:38:05 +0100

    R# rename fields

--- src/LazyStorage/Json/JsonStorage.cs
+++ src/LazyStorage/Json/JsonStorage.cs
@@ -5,42 +5,42 @@ namespace LazyStorage.Json
 {
     internal class JsonStorage : IStorage
     {
-        private readonly string m_StorageFolder;
-        private Dictionary<string, IRepository> m_Repos;
+        private readonly string _storageFolder;
+        private Dictionary<string, IRepository> _repos;
 
         public JsonStorage(string storageFolder)
         {
-            m_Repos = new Dictionary<string, IRepository>();
-            m_StorageFolder = storageFolder;
+            _repos = new Dictionary<string, IRepository>();
+            _storageFolder = storageFolder;
         }
 
         public IRepository<T> GetRepository<T>() where T : IStorable<T>, new()
         {
             var typeAsString = typeof (T).ToString();
 
-            if (!m_Repos.ContainsKey(typeAsString))
+            if (!_repos.ContainsKey(typeAsString))
             {
-                m_Repos.Add(typeAsString, new JsonRepository<T>(m_StorageFolder));
+                _repos.Add(typeAsString, new JsonRepository<T>(_storageFolder));
             }
 
-            return (IRepository<T>) m_Repos[typeAsString];
+            return (IRepository<T>) _repos[typeAsString];
         }
 
         public IRepository<T> GetRepository<T>(IConverter<T> converter)
         {
             var typeAsString = typeof(T).ToString();
 
-            if (!m_Repos.ContainsKey(typeAsString))
+            if (!_repos.ContainsKey(typeAsString))
             {
-                m_Repos.Add(typeAsString, new JsonRepositoryWithConverter<T>(m_StorageFolder, converter));
+                _repos.Add(typeAsString, new JsonRepositoryWithConverter<T>(_storageFolder, converter));
             }
 
-            return (IRepository<T>)m_Repos[typeAsString];
+            return (IRepository<T>)_repos[typeAsString];
         }
 
         public void Save()
         {
-            foreach (var repository in m_Repos)
+            foreach (var repository in _repos)
             {
                 repository.Value.Save();
             }
@@ -48,7 +48,7 @@ namespace LazyStorage.Json
 
         public void Discard()
         {
-            foreach (var repository in m_Repos)
+            foreach (var repository in _repos)
             {
                 repository.Value.Load();
             }
