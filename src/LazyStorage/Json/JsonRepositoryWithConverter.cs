e3aa8b6f7b584b79d8728dcb7836c136bf8f6a77
David Eadie <david.eadie@red-gate.com>
2017-10-27 16:35:41 +0100

    R# rename fields

--- src/LazyStorage/Json/JsonRepositoryWithConverter.cs
+++ src/LazyStorage/Json/JsonRepositoryWithConverter.cs
@@ -9,59 +9,59 @@ namespace LazyStorage.Json
 {
     internal class JsonRepositoryWithConverter<T> : IRepository<T>
     {
-        private readonly string m_Uri;
-        private List<T> m_Repository = new List<T>();
-        private readonly string m_StorageFolder;
-        private readonly IConverter<T> m_Converter;
+        private readonly string _uri;
+        private List<T> _repository = new List<T>();
+        private readonly string _storageFolder;
+        private readonly IConverter<T> _converter;
 
         public JsonRepositoryWithConverter(string storageFolder, IConverter<T> converter)
         {
-            m_Uri = $"{storageFolder}{typeof(T)}.json";
-            m_StorageFolder = storageFolder;
-            m_Converter = converter;
+            _uri = $"{storageFolder}{typeof(T)}.json";
+            _storageFolder = storageFolder;
+            _converter = converter;
             Load();
         }
 
         public ICollection<T> Get(Func<T, bool> exp = null)
         {
-            return exp != null ? m_Repository.Where(exp).ToList() : m_Repository.ToList();
+            return exp != null ? _repository.Where(exp).ToList() : _repository.ToList();
         }
 
         public void Set(T item)
         {
-            var storableObject = m_Converter.GetStorableObject(item);
-            var matchingItemsInStore = m_Repository.Where(x => m_Converter.IsEqual(storableObject, x));
+            var storableObject = _converter.GetStorableObject(item);
+            var matchingItemsInStore = _repository.Where(x => _converter.IsEqual(storableObject, x));
 
             if (matchingItemsInStore.Any())
             {
                 // Update
-                m_Repository.Remove(matchingItemsInStore.First());
-                m_Repository.Add(item);
+                _repository.Remove(matchingItemsInStore.First());
+                _repository.Add(item);
             }
             else
             {
                 // Insert
-                m_Repository.Add(item);
+                _repository.Add(item);
             }
         }
 
         public void Delete(T item)
         {
-            var storableObject = m_Converter.GetStorableObject(item);
-            var obj = m_Repository.Where(x => m_Converter.IsEqual(storableObject, x));
-            m_Repository.Remove(obj.First());
+            var storableObject = _converter.GetStorableObject(item);
+            var obj = _repository.Where(x => _converter.IsEqual(storableObject, x));
+            _repository.Remove(obj.First());
         }
 
 
         public object Clone()
         {
-            var newRepo = new JsonRepositoryWithConverter<T>(m_StorageFolder, m_Converter);
+            var newRepo = new JsonRepositoryWithConverter<T>(_storageFolder, _converter);
 
             foreach (var item in Get())
             {
-                var info = m_Converter.GetStorableObject(item);
+                var info = _converter.GetStorableObject(item);
 
-                var temp = m_Converter.GetOriginalObject(info);
+                var temp = _converter.GetOriginalObject(info);
 
                 newRepo.Set(temp);
             }
@@ -71,26 +71,26 @@ namespace LazyStorage.Json
         }
         public void Load()
         {
-            if (File.Exists(m_Uri))
+            if (File.Exists(_uri))
             {
-                var jsonContent = File.ReadAllText(m_Uri);
+                var jsonContent = File.ReadAllText(_uri);
                 var convertedItems = JsonConvert.DeserializeObject<List<StorableObject>>(jsonContent);
 
-                m_Repository = convertedItems.Select(x => m_Converter.GetOriginalObject(x)).ToList();
+                _repository = convertedItems.Select(x => _converter.GetOriginalObject(x)).ToList();
             }
             else
             {
-                m_Repository = new List<T>();
+                _repository = new List<T>();
             }
 
         }
 
         public void Save()
         {
-            var convertedItems = m_Repository.Select(item => m_Converter.GetStorableObject(item)).ToList();
+            var convertedItems = _repository.Select(item => _converter.GetStorableObject(item)).ToList();
 
             var fileContent = JsonConvert.SerializeObject(convertedItems, Formatting.Indented);
-            File.WriteAllText(m_Uri, fileContent);
+            File.WriteAllText(_uri, fileContent);
         }
     }
 }
