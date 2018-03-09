19ab0b3b056e7a4a6d8d8969f64fc6827f840cd9
David Eadie <david.eadie@red-gate.com>
2018-03-09 15:29:16 +0000

    Add test that we can update objects across saves

--- src/LazyStorage.Tests/PersistenceTests.cs
+++ src/LazyStorage.Tests/PersistenceTests.cs
@@ -75,13 +75,13 @@ namespace LazyStorage.Tests
 
             var repo = dal.GetRepository(converter);
 
-            var obj1 = new TestObject {Name = "Test"};
+            var obj1 = new TestObject {Name = "First Object"};
 
             repo.Set(obj1);
             dal.Save();
 
             // Make some changes
-            var obj2 = new TestObject {Name = "Test"};
+            var obj2 = new TestObject {Name = "Object To Be Discarded"};
 
             // Update the object in the repo but discard changes
             repo.Set(obj2);
@@ -92,6 +92,36 @@ namespace LazyStorage.Tests
 
             var testObject = repo2.Get().Single();
             Assert.True(testObject.ContentEquals(obj1), "The object changes were not reverted in the repo");
+        }
+
+        [Theory, MemberData(nameof(StorageTypes))]
+        public void StorageIsOverWrittenIfSavedMultipleTimes(ITestStorage storage)
+        {
+            _currentStorage = storage;
+
+            // Insert into the repo
+            var dal = storage.GetStorage();
+            var converter = new TestObjectStorageConverter();
+
+            var repo = dal.GetRepository(converter);
+
+            var obj1 = new TestObject { Name = "Test", StartDate = DateTime.Now };
+
+            repo.Set(obj1);
+            dal.Save();
+
+            // Make some changes
+            var obj2 = new TestObject { Name = "Test", EndDate = DateTime.Now };
+
+            // Update the object in the repo but discard changes
+            repo.Set(obj2);
+            dal.Save();
+
+            var dal2 = storage.GetStorage();
+            var repo2 = dal2.GetRepository(converter);
+
+            var testObject = repo2.Get().Single();
+            Assert.True(testObject.ContentEquals(obj2), "The object was not updated in the repo");
         }
 
         public void Dispose()
